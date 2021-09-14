c1d2d235f32cc4d0795ee39a810b888b284fa287
Danilo Portela <danilo.portela.ext@dasa.com.br>
2021-09-14 00:58:40 -0300

    Classes para o teste de Danilo Nogueira Portela

--- src/main/java/br/com/alelo/consumer/consumerpat/entity/Consumer.java
+++ src/main/java/br/com/alelo/consumer/consumerpat/entity/Consumer.java
@@ -1,70 +1,40 @@
 package br.com.alelo.consumer.consumerpat.entity;
 
 
-import jdk.jfr.DataAmount;
-import lombok.Data;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.*;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import java.util.Date;
-import java.util.Objects;
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.LocalDate;
 
 
 @Data
 @Entity
-public class Consumer {
+@Builder
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+public class Consumer implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    Integer id;
-    String name;
-    int documentNumber;
-    Date birthDate;
+    private Integer id;
 
-    //contacts
-    int mobilePhoneNumber;
-    int residencePhoneNumber;
-    int phoneNumber;
-    String email;
+    @Column(name = "NAME")
+    private String name;
 
-    //Address
-    String street;
-    int number;
-    String city;
-    String country;
-    int portalCode;
+    @Column(name = "DOCUMENT_NUMBER")
+    private String documentNumber;
 
-    //cards
-    int foodCardNumber;
-    double foodCardBalance;
+    @Column(name = "BIRTH_DATE")
+    private LocalDate birthDate;
 
-    int fuelCardNumber;
-    double fuelCardBalance;
+    @Embedded
+    private Contact contact;
 
-    int drugstoreNumber;
-    double drugstoreCardBalance;
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Consumer consumer = (Consumer) o;
-        return documentNumber == consumer.documentNumber
-                && mobilePhoneNumber == consumer.mobilePhoneNumber
-                && residencePhoneNumber == consumer.residencePhoneNumber
-                && phoneNumber == consumer.phoneNumber
-                && number == consumer.number
-                && portalCode == consumer.portalCode
-                && foodCardNumber == consumer.foodCardNumber
-                && Double.compare(consumer.foodCardBalance, foodCardBalance) == 0
-                && fuelCardNumber == consumer.fuelCardNumber && Double.compare(consumer.fuelCardBalance, fuelCardBalance) == 0
-                && drugstoreNumber == consumer.drugstoreNumber && Double.compare(consumer.drugstoreCardBalance, drugstoreCardBalance) == 0
-                && Objects.equals(id, consumer.id) && Objects.equals(name, consumer.name) && Objects.equals(birthDate, consumer.birthDate)
-                && Objects.equals(email, consumer.email) && Objects.equals(street, consumer.street) && Objects.equals(city, consumer.city)
-                && Objects.equals(country, consumer.country);
-    }
-
+    @Embedded
+    private Address address;
 
 }
